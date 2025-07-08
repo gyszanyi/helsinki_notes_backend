92ea54e4e851b64d3e5f7dd521996da605c05333
Gyöngyvér Szanyi <gyongyver.szanyi@gmail.com>
2025-07-08 14:25:59 +0200

    validation of schema and updated error handling

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(requestLogger)
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
 
 /*
 const mongoose = require('mongoose')
@@ -77,57 +77,45 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-/*
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-  */
-
-app.get('/api/notes/:id', (request, response) => {
- Note.findById(request.params.id).then(note => {
-    response.json(note) 
- })
-})
- 
-app.put('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
- 
-  if (note) {
-    const changedNote = {...note, important: !note.important}
-    notes = notes.filter(note => note.id !== id)
-    notes = notes.concat(changedNote)
-    const newNote = notes.find(note => note.id === id)
-    response.json(newNote)
-  } else {
-    response.status(404).end()
-  }
-}) 
-
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
+app.put('/api/notes/:id', (request, response, next) => {
+ const { content, important } = request.body
+  Note.findById(request.params.id)
+  .then(note => {
+    if (!note) {
+      return response.status(404).end()
+    }
+    note.content = content
+    note.important = important
+    return note.save().then((updatedNote) => {
+      response.json(updatedNote)
     })
-  }
+  })
+  .catch(error => next(error))
+})
+
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
@@ -137,14 +125,28 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
-
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
